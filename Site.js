620cd5865e8c9bbdb90bbd9ac0eb4bc1701abbc1
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-06-10 18:42:16 -0300

    Add files via upload

--- Site.js
+++ Site.js
@@ -35,4 +35,36 @@ const changeFavicon = link => {
       $favicon.href = link
       document.head.appendChild($favicon)
     }
-  }
+}
+
+
+function updateClock() {
+    var startDateTime = new Date(2020,7,10,23,59,59,0); // YYYY (M-1) D H m s ms (start time and date from DB)
+    var startStamp = startDateTime.getTime();
+    
+    var newDate = new Date();
+    var newStamp = newDate.getTime();
+    
+    var timer; 
+
+    newDate = new Date();
+    newStamp = newDate.getTime();
+    let diff = Math.round((newStamp-startStamp)/1000);
+    
+    let year = Math.floor(diff/(12*30*24*60*60)); 
+    diff = diff-(12*year*30*24*60*60);
+    let month = Math.floor(diff/(30*24*60*60)); 
+    diff = diff-(30*month*24*60*60);
+    let days = Math.floor(diff/(24*60*60)); 
+    diff = diff-(days*24*60*60);
+    let hours = Math.floor(diff/(60*60));
+    diff = diff-(hours*60*60);
+    let minutes = Math.floor(diff/(60));
+    diff = diff-(minutes*60);
+    let seconds = diff;
+    
+
+    document.getElementById("time-elapsed").innerHTML = year+" year(s), " + month+" month(s), "+ days+" day(s), "+hours+" hour(s), "+minutes+" minute(s), "+seconds+" second(s)";
+}
+
+timer = setInterval(updateClock, 1000);
